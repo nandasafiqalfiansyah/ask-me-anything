aa9d6404a22b7bfc198fe0bca4f3c5ad1169a6da
Nanda Safiq Alfiansyah <nandasafiqalfiansyah@gmail.com>
2024-11-23 21:43:34 +0700

    [fix] :bug: update page experient

--- components/recent-edu.tsx
+++ components/recent-edu.tsx
@@ -1,13 +1,21 @@
-import Link from 'next/link'
+'use client'
 
+import React from 'react'
+import Link from 'next/link'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 // Array of post data
 const posts = [
   {
     title: 'Universitas Muhammadiyah Ponorogo',
     summary: 'Higher Education Institution in Ponorogo, East Java, Indonesia',
     publishedAt: '11-13-2024',
-    logo: 'https://media.licdn.com/dms/image/v2/C560BAQFnc-z5aubSFw/company-logo_200_200/company-logo_200_200/0/1646043826382/universitas_muhammadiyah_ponorogo_logo?e=1739404800&v=beta&t=Foxvx4y7N2RVSVaKDiJe8gR6BQ8DoDQcW0rKf0eswsE',
-    link: 'https://www.linkedin.com/school/universitas-muhammadiyah-ponorogo'
+    logo: 'https://umpo.ac.id/web-con/app/app-upload/images/files/1686106386-UMPO-logo-resmi.png',
+    link: 'https://www.linkedin.com/school/universitas-muhammadiyah-ponorogo',
+    tasks: [
+      'Conducted student counseling sessions',
+      'Managed academic events and workshops',
+      'Developed e-learning materials'
+    ]
   }
 ]
 
@@ -25,38 +33,49 @@ export default function RecentEdu() {
   return (
     <section className='pb-24'>
       <div>
-        <h2 className='mb-12 text-3xl font-bold'>Education</h2>
+        <h2 className='mb-12 text-center text-3xl font-bold md:text-left'>
+          Education
+        </h2>
 
         <ul className='flex flex-col gap-8'>
           {posts.map(post => (
-            <li key={post.link} className='flex items-center justify-between'>
-              <Link href={post.link} className='flex items-center gap-6'>
+            <li
+              key={post.link}
+              className='flex flex-col gap-4 md:flex-row md:items-center md:justify-between'
+            >
+              {/* Main Content */}
+              <div className='flex flex-col gap-4 md:flex-row md:items-center md:gap-6'>
                 <img
                   src={post.logo}
                   alt={`${post.title} logo`}
-                  className='h-16 w-16 flex-shrink-0 object-contain' // Lebar dan tinggi tetap
+                  className='h-16 w-16 flex-shrink-0 object-contain md:h-20 md:w-20'
                 />
-                <div className='max-w-lg'>
+                <div>
                   <p className='text-lg font-semibold'>{post.title}</p>
                   <p className='mt-1 text-sm font-light text-muted-foreground'>
                     {post.summary}
                   </p>
                 </div>
-              </Link>
+              </div>
+
+              {/* Date */}
               {post.publishedAt && (
-                <p className='whitespace-nowrap text-sm font-light'>
+                <p className='text-sm font-light text-gray-500 md:text-right'>
                   {formatDate(post.publishedAt)}
                 </p>
               )}
             </li>
           ))}
         </ul>
-        <Link
-          href='/'
-          className='mt-8 inline-flex items-center gap-2 text-muted-foreground underline decoration-1 underline-offset-2 transition-colors hover:text-foreground'
-        >
-          <span>All Education Experience</span>
-        </Link>
+
+        <div className='mt-8 text-center md:text-left'>
+          <Link
+            href='/'
+            className='inline-flex items-center gap-2 text-sm font-medium text-muted-foreground underline decoration-1 underline-offset-2 transition-colors hover:text-foreground'
+          >
+            <span>All Education Experience</span>
+          </Link>
+        </div>
       </div>
     </section>
   )
--- components/recent-work.tsx
+++ components/recent-work.tsx
@@ -3,19 +3,26 @@ import Link from 'next/link'
 // Array of post data
 const posts = [
   {
-    title: 'Software Engineer',
+    title: 'Lintasarta Cloudeka Digischool 2023',
     summary:
-      'Worked on the Google Search team to improve search results for users.',
+      'Contributed to team-based final projects, demonstrating proficiency in cloud computing concepts',
+    publishedAt: '2020-06-01',
+    logo: 'https://media.licdn.com/dms/image/C560BAQEuItk7F_3JPQ/company-logo_200_200/0/1630645857701/lintasartacloudeka_logo?e=2147483647&v=beta&t=IdNaLjKgzVCTi4-BKH5Guud4VlRqjkRKrKarPgB9954',
+    link: 'https://www.linkedin.com/company/bangkit-academy/'
+  },
+  {
+    title: 'Bangkit Academy 2023',
+    summary:
+      'Contributed to team-based final projects, demonstrating proficiency in cloud computing concepts',
     publishedAt: '2020-06-01',
     logo: 'https://media.licdn.com/dms/image/v2/C560BAQEVREspL4ipDQ/company-logo_100_100/company-logo_100_100/0/1630661916225/bangkit_academy_logo?e=1737590400&v=beta&t=wynG8aNPeWscmfCtqIUjnGDZVDRI0pDLh-s1Mtngbak',
     link: 'https://www.linkedin.com/company/bangkit-academy/'
   },
   {
-    title: 'Software Engineer',
-    summary:
-      'Worked on the Google Search team to improve search results for users.',
+    title: 'Student Internship Kariermu',
+    summary: 'Learning to be a proficient receptionist for customers.',
     publishedAt: '2020-06-01',
-    logo: 'https://media.licdn.com/dms/image/v2/C560BAQEVREspL4ipDQ/company-logo_100_100/company-logo_100_100/0/1630661916225/bangkit_academy_logo?e=1737590400&v=beta&t=wynG8aNPeWscmfCtqIUjnGDZVDRI0pDLh-s1Mtngbak',
+    logo: 'https://cdn.sekolah.mu/upload/branding--4df889fc3ca549f692d8f354b155b1561644804821.png',
     link: 'https://www.linkedin.com/company/bangkit-academy/'
   },
   {
@@ -23,7 +30,7 @@ const posts = [
     summary:
       'During a 3-month internship, I served as a full-stack developer for the Ponorogo District Development',
     publishedAt: '2023-08-01',
-    logo: 'https://w7.pngwing.com/pngs/329/450/png-transparent-dinas-pendidikan-ponorogo-logo-regency-reog-padi-kapas-city-ponorogo-regency-padi-kapas.png',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/4/4b/Lambang_Kabupaten_Ponorogo.png',
     link: 'https://www.linkedin.com/company/bangkit-academy/'
   }
 ]
@@ -42,38 +49,49 @@ export default function RecentWork() {
   return (
     <section className='pb-24'>
       <div>
-        <h2 className='mb-12 text-3xl font-bold'>Work Experience</h2>
+        <h2 className='mb-12 text-center text-3xl font-bold md:text-left'>
+          Experience
+        </h2>
 
         <ul className='flex flex-col gap-8'>
           {posts.map(post => (
-            <li key={post.link} className='flex items-center justify-between'>
-              <Link href={post.link} className='flex items-center gap-6'>
+            <li
+              key={post.link}
+              className='flex flex-col gap-4 md:flex-row md:items-center md:justify-between'
+            >
+              {/* Main Content */}
+              <div className='flex flex-col gap-4 md:flex-row md:items-center md:gap-6'>
                 <img
                   src={post.logo}
                   alt={`${post.title} logo`}
-                  className='h-16 w-16 flex-shrink-0 object-contain' // Lebar dan tinggi tetap
+                  className='h-16 w-16 flex-shrink-0 object-contain md:h-20 md:w-20'
                 />
-                <div className='max-w-lg'>
+                <div>
                   <p className='text-lg font-semibold'>{post.title}</p>
                   <p className='mt-1 text-sm font-light text-muted-foreground'>
                     {post.summary}
                   </p>
                 </div>
-              </Link>
+              </div>
+
+              {/* Date */}
               {post.publishedAt && (
-                <p className='whitespace-nowrap text-sm font-light'>
+                <p className='text-sm font-light text-gray-500 md:text-right'>
                   {formatDate(post.publishedAt)}
                 </p>
               )}
             </li>
           ))}
         </ul>
-        <Link
-          href='/'
-          className='mt-8 inline-flex items-center gap-2 text-muted-foreground underline decoration-1 underline-offset-2 transition-colors hover:text-foreground'
-        >
-          <span>All Work Experience</span>
-        </Link>
+
+        <div className='mt-8 text-center md:text-left'>
+          <Link
+            href='/'
+            className='inline-flex items-center gap-2 text-sm font-medium text-muted-foreground underline decoration-1 underline-offset-2 transition-colors hover:text-foreground'
+          >
+            <span>All Work Experience</span>
+          </Link>
+        </div>
       </div>
     </section>
   )
