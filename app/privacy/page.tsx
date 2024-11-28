330da43f6bf7c5146878b1cc3f8f12514ebf0a0c
Nanda Safiq Alfiansyah <nandasafiqalfiansyah@gmail.com>
2024-11-28 11:33:45 +0700

    [feat] prvacy police page

---
+++ app/privacy/page.tsx
@@ -0,0 +1,96 @@
+export default function Component() {
+  return (
+    <div className='py-6 pb-24 pt-40'>
+      <div className='container grid max-w-2xl gap-10 px-4 md:px-6 lg:gap-16 xl:max-w-3xl xl:gap-20'>
+        <div className='space-y-2'>
+          <h1 className='text-3xl font-bold tracking-tighter sm:text-5xl md:text-6xl'>
+            Privacy Policy
+          </h1>
+          <p className='text-gray-500 dark:text-gray-400'>
+            Last updated: September 2024
+          </p>
+        </div>
+        <div className='space-y-6'>
+          <div className='space-y-3'>
+            <h2 className='text-2xl font-bold tracking-tight'>
+              Introduction and Scope
+            </h2>
+            <p className='text-gray-500 dark:text-gray-400'>
+              We respect your privacy and are committed to protecting your
+              personal data. This privacy policy will inform you as to how we
+              look after your personal data when you visit our website
+              (regardless of where you visit it from) and tell you about your
+              privacy rights and how the law protects you.
+            </p>
+          </div>
+          <div className='space-y-3'>
+            <h2 className='text-2xl font-bold tracking-tight'>
+              Data Collection
+            </h2>
+            <p className='text-gray-500 dark:text-gray-400'>
+              We may collect, use, store, and transfer different kinds of
+              personal data about you which we have grouped together as follows:
+            </p>
+            <ul className='list-inside list-disc space-y-1'>
+              <li>Identity Data includes first name and last name.</li>
+              <li>
+                Contact Data includes email address and telephone numbers.
+              </li>
+              <li>
+                Technical Data includes internet protocol (IP) address, your
+                login data, browser type and version, time zone setting and
+                location, browser plug-in types and versions, operating system
+                and platform, and other technology on the devices you use to
+                access this website.
+              </li>
+            </ul>
+          </div>
+          <div className='space-y-3'>
+            <h2 className='text-2xl font-bold tracking-tight'>
+              Use of Cookies
+            </h2>
+            <p className='text-gray-500 dark:text-gray-400'>
+              Our website uses cookies to distinguish you from other users of
+              our website. This helps us to provide you with a good experience
+              when you browse our website and also allows us to improve our
+              site.
+            </p>
+          </div>
+          <div className='space-y-3'>
+            <h2 className='text-2xl font-bold tracking-tight'>
+              Data Storage and Security
+            </h2>
+            <p className='text-gray-500 dark:text-gray-400'>
+              The security of your personal data is important to us. We have put
+              in place appropriate security measures to prevent your personal
+              data from being accidentally lost, used, or accessed in an
+              unauthorized way, altered, or disclosed.
+            </p>
+          </div>
+          <div className='space-y-3'>
+            <h2 className='text-2xl font-bold tracking-tight'>Data Sharing</h2>
+            <p className='text-gray-500 dark:text-gray-400'>
+              We may have to share your data with third parties, including
+              third-party service providers and other entities in the group.
+            </p>
+          </div>
+          <div className='space-y-3'>
+            <h2 className='text-2xl font-bold tracking-tight'>User Rights</h2>
+            <p className='text-gray-500 dark:text-gray-400'>
+              You have the right to make a complaint at any time to the
+              Information Commissioner's Office (ICO), the UK supervisory
+              authority for data protection issues
+            </p>
+          </div>
+          <div className='space-y-3'>
+            <h2 className='text-2xl font-bold tracking-tight'>Contact Us</h2>
+            <p className='text-gray-500 dark:text-gray-400'>
+              If you have any questions about this privacy policy, please
+              contact us.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
